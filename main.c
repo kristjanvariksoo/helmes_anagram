6882478e845ab2d1410cd6cf700b93b7389790f4
Kristjan Variksoo <kristjan.variksoo@gmail.com>
2019-02-23 12:43:01 +0200

    Output follows standard and handles unicode charachters, input does as well.

--- main.c
+++ main.c
@@ -8,6 +8,74 @@
 #include <iconv.h>
 #include <unistd.h>
 
+void debug_string(char *str) {
+  printf("\n%d[%c]",str[0], str[0]);
+  for (int i = 1; i < strlen(str); i++)
+  {
+    printf(" - %d[%c]",str[i], str[i]);
+  }
+  printf("\n");
+  return;
+}
+
+//https://codereview.stackexchange.com/a/40792
+size_t iso8859_1_to_utf8(char *content, size_t max_size)
+{
+    char *src, *dst;
+
+    //first run to see if there's enough space for the new bytes
+    for (src = dst = content; *src; src++, dst++)
+    {
+        if (*src & 0x80)
+        {
+            // If the high bit is set in the ISO-8859-1 representation, then
+            // the UTF-8 representation requires two bytes (one more than usual).
+            ++dst;
+        }
+    }
+
+    if (dst - content + 1 > max_size)
+    {
+        // Inform caller of the space required
+        return dst - content + 1;
+    }
+
+    *(dst + 1) = '\0';
+    while (dst > src)
+    {
+        if (*src & 0x80)
+        {
+            *dst-- = 0x80 | (*src & 0x3f);                     // trailing byte
+            *dst-- = 0xc0 | (*((unsigned char *)src--) >> 6);  // leading byte
+        }
+        else
+        {
+            *dst-- = *src--;
+        }
+    }
+    return 0;  // SUCCESS
+}
+
+//https://codereview.stackexchange.com/a/40857
+char* iso88959_to_utf8(const char *str)
+{
+    char *utf8 = malloc(1 + (2 * strlen(str)));
+
+    if (utf8) {
+        char *c = utf8;
+        for (; *str; ++str) {
+            if (*str & 0x80) {
+                *c++ = *str;
+            } else {
+                *c++ = (char) (0xc0 | (unsigned) *str >> 6);
+                *c++ = (char) (0x80 | (*str & 0x3f));
+            }
+        }
+        *c++ = '\0';
+    }
+    return utf8;
+}
+
 //https://stackoverflow.com/a/11173493
 /* UTF-8 to ISO-8859-1/ISO-8859-15 mapper.
  * Return 0..255 for valid ISO-8859-15 code points, 256 otherwise.
@@ -264,9 +332,32 @@ string[fsize] = 0;
     //printf("After Thread\n");
     //printf("There were %d matches\n", possible_len);
     //printf("%d\n", strlen("\n"));
+
+    //OUTPUT
     long end = getMicrotime();
-    printf("It took %d microseconds\n", (end-start));
+    printf("%d", (end-start));
+    setlocale(LC_ALL, "");
+    for (int i = 0; i < possible_len; i++) {
+      char a[30];
+      strcpy(a, possible[i]);
+      iso8859_1_to_utf8(a, 100);
+      printf(",%s", a);
+
+/*    char *a = "õnn";
+      char b[10];
+      char c[10];
+
+      strcpy(c, b);
+      debug_string(a);
+      debug_string(b);
+      debug_string(iso88959_to_utf8(b));
+
+      debug_string(c);*/
+
+    }
 
 
+
+    printf("\n");
     exit(0);
 }
