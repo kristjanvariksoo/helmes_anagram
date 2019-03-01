1dd2bda639c8aea36a660425a2e43f9a6663774d
Kristjan Variksoo <kristjan.variksoo@gmail.com>
2019-03-01 19:39:42 +0200

    Holy shit it actually works.

--- main.c
+++ main.c
@@ -13,7 +13,19 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void debug_string(char * str) {
+char *strlwr(char *str)
+{
+  unsigned char *p = (unsigned char *)str;
+
+  while (*p) {
+     *p = tolower((unsigned char)*p);
+      p++;
+  }
+
+  return str;
+}
+
+void debug_string(unsigned char * str) {
     printf("\n%d[%c]", str[0], str[0]);
     for (int i = 1; i < strlen(str); i++) {
         printf(" - %i[%c]", str[i], str[i]);
@@ -22,30 +34,17 @@ void debug_string(char * str) {
     return;
 }
 
-char lowerIfHigher(char in) {
+char lowerIfHigher(unsigned char in) {
   char out;
-  out = in;
-  wprintf("lowerIfHigher %i[%c]\n", in, in);
-  wchar_t star = 0x2605;
-  wprintf(L"%lc\n", star);
-  wprintf(L"%lc\n", star);
-  wprintf(L"%lc\n", star);
-  if (in < 91) { //Uppercase ascii or sth special
-    if (in > 0) {
-      out = in+32;
-      printf("lowerIfHigher %i[%c] => %i[%c]\n", in, in, out, out);
-    } else {
-      wchar_t star = 0x2605;
-      wprintf(L"%lc\n", star);
-      wprintf(L"%lc\n", star);
-      wprintf(L"%lc\n", star);
-      wprintf(L"%lc\n", star);
-      wprintf(L"%lc\n", star);
-      wprintf(L"%lc\n", star);
 
-      printf("lowerIfHigher %i[%c] => %i[%c]\n", in, in, in+32, in+32);
-    }
+  if (in > 40 && in < 91) {
+    //ASCII capital
+    out = in + 32;
+    //printf ("%d - %c => %d - %c\n", in, in, out, out);
+  } else {
+    out = in;
   }
+
   return out;
 }
 
@@ -77,26 +76,52 @@ int main(int argc, char ** argv) {
 
     nice(-20);
 
-    int in_len = strlen(argv[2]);
-    char *in;
+
+    int in_len;
+    if (argc == 3) {
+      in_len = strlen(argv[2]);
+    } else if (argc == 4) {
+      in_len = strlen(argv[2]) + 1 + strlen(argv[3]);
+    } else {
+      exit(EXIT_FAILURE);
+    }
+
+    unsigned char *in;
     in = malloc(in_len);
-    memcpy(in, argv[2], in_len);
+
+    if (argc == 3) {
+      memcpy(in, argv[2], in_len);
+    } else if (argc == 4) {
+      memcpy(in, argv[2], in_len);
+      strcat(in, " ");
+      strcat(in, argv[3]);
+    } else {
+      exit(EXIT_FAILURE);
+    }
+
+    in = strlwr(in);
+
 /*    lowerIfHigher(in[0]);*/
-    char in_ord[30];
+    unsigned char in_ord[50];
     strcpy(in_ord, in );
     qsort(in_ord, strlen(in_ord), sizeof(char), cmp);
 
-    char possible[106000][30]; //array for all correct anagrams
+
+    unsigned char possible[106000][50]; //array for all correct anagrams
 
     int in_sum = 0;
     for (int i = 0; i < in_len; i++) {
-        in_sum += (int) in [i];
+      in[i] = tolower(in[i]);
+      in_sum += (int) in [i];
+    //  printf ("%d - %c\n", in[i], in[i]);
     }
 
-    char s_ord[30];
+    //printf("|%s| - |%s| - %d\n", in, in_ord, in_sum);
+
+    unsigned char s_ord[50];
     int possible_len = 0;
 
-    char *buffer;
+    unsigned char *buffer;
     struct stat stat;
 
     int fd = open(argv[1], O_RDONLY);
@@ -104,35 +129,35 @@ int main(int argc, char ** argv) {
     fstat(fd, &stat);
     /* PROT_READ disallows writing to buffer: will segv */
 
-    char *text;
+    unsigned char *text;
     buffer = mmap(0, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     if ( buffer != (void*)-1 ) {
       text = malloc(stat.st_size);
       memcpy(text, buffer, stat.st_size);
     }
 
-
-
-    char *line;
-    char *next_line = strtok(text, "\r\n");
+    unsigned char *line;
+    unsigned char *next_line = strtok(text, "\r\n");
     while (next_line != NULL) {
       int line_len = 0;
       line = next_line;
       next_line = strtok(NULL, "\r\n");
 
-
-
-
       //check if length is same
       line_len = next_line - line - 2;
-      //printf("%d - %d\n", line_len, in_len);
-      if (line_len == in_len) {
 
+      //printf("%s - %d - %d\n", line, line_len, in_len);
+      if (line_len == in_len) {
         //if sum is same
+        //printf("SAME: %s - %d - %d\n", line, line_len, in_len);
+        line[0] = lowerIfHigher(line[0]);
         int s_sum = 0;
         for (int i = 0; i < line_len; i++) {
-            s_sum += (int) line[i];
+          line[i] = tolower(line[i]);
+          s_sum += (int) line[i];
+          //printf ("%d - %c\n", line[i], line[i]);
         }
+        //printf("%s - %d - %d\n", line, s_sum, in_sum);
         if (s_sum == in_sum) {
           //printf("%s - %d - %d\n", line, s_sum, in_sum);
           //if sorted are identical
@@ -143,7 +168,6 @@ int main(int argc, char ** argv) {
 
           if (strcmp(s_ord, in_ord) == 0) {
               strcpy(possible[possible_len], line);
-
               possible_len++;
           }
         }
